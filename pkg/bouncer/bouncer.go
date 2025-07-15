a3787a343dd472ecb9a7b2553ad166b05820a6e2
LessForOne <neverthelessforone@gmail.com>
2025-07-15 13:10:41 +0200

    minor changes

--- pkg/bouncer/bouncer.go
+++ pkg/bouncer/bouncer.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Bouncer() error {
-	cmd := exec.Command("sudo", "apt", "install", "crowdsec-firewall-bouncer-iptables")
+	cmd := exec.Command("sudo", "apt", "install", "-y", "crowdsec-firewall-bouncer-iptables")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
